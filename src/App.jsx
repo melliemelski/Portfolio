c87b75b1187d99632a5831fc7cf8c3409ee6a6f3
mel <melissa.sargent@hotmail.co.uk>
2020-07-09 19:09:44 +0100

    first setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import styles from "./App.module.scss";
+
+class App extends Component {
+  state = {  }
+  render() { 
+    return ( 
+    <>
+    <Router />
+    </> 
+    );
+  }
+}
+ 
+export default App;
---
+++ src/Containers/Routes/Routes.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { Router } from "@reach/router";
+
+import Dashboard from "../Dashboard";
+import Cookbook from "../Cookbook";
+import NotFound from "../../components/NotFound";
+
+class Routes extends Component {
+  render() {
+    
+    return (
+      <Router>
+       
+        
+      </Router>
+    );
+  }
+}
+
+export default Routes;
