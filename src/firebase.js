5060521d7fc8bbcc49b353cade5bdc65a737fbb3
mel <melissa.sargent@hotmail.co.uk>
2020-07-17 15:33:31 +0100

    github repo setup

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import * as firebase from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBkgUgk21rQDLJuyeMFUnT4IzTPJYn4ATg",
+  authDomain: "portfolio-a9e65.firebaseapp.com",
+  databaseURL: "https://portfolio-a9e65.firebaseio.com",
+  projectId: "portfolio-a9e65",
+  storageBucket: "portfolio-a9e65.appspot.com",
+  messagingSenderId: "360533516878",
+  appId: "1:360533516878:web:f9b17e7ed29a5a820c191f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
