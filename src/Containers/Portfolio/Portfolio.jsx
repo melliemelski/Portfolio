82862a92ea2c114914f4ad1423fe6728d11ff360
mel <melissa.sargent@hotmail.co.uk>
2020-07-21 21:19:36 +0100

    more styling on work page

--- src/Containers/Portfolio/Portfolio.jsx
+++ src/Containers/Portfolio/Portfolio.jsx
@@ -8,19 +8,26 @@ class Portfolio extends Component {
   render() { 
     return ( 
       <>
-      <h2 className={styles.header}>My Work</h2>
-      <section className={styles.workContainer}>
-          
-          <div className={`${styles.frame} ${styles.frame1}`}>
-            <p>Nature Memory Game<br></br> Week 4 Game made using Javascript</p>
-            <div className={styles.link}><a href=""><FontAwesomeIcon icon={faGithub} /></a></div>
-          </div>
-          <div className={`${styles.frame} ${styles.frame2}`} >
-          <p>BrewDog API</p><p>React App built with an API call</p>
-          </div>
-      
-      </section>
-    </>
+        <section className={styles.container}>
+            <h2 className={styles.header}>My Work</h2>
+            <section className={styles.workContainer}> 
+                <div className={`${styles.frame} ${styles.frame1}`} >
+                  <p>Ewe 4 Ewe<br></br>Client Project</p>
+                  <div className={styles.link}><a href="https://github.com/nology-tech/ewe4you" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></div>
+                </div>
+
+                <div className={`${styles.frame} ${styles.frame2}`} >
+                  <p>BrewDog API<br></br>React App built with an API call</p>
+                  <div className={styles.link}><a href="https://github.com/melliemelski/punk-api-challenge" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></div>
+                </div>
+                
+                <div className={`${styles.frame} ${styles.frame3}`}>
+                  <p>Nature Memory Game<br></br> Week 4 Game made using Javascript</p>
+                  <div className={styles.link}><a href="https://github.com/melliemelski/wk4-game" target="_blank"><FontAwesomeIcon icon={faGithub} /></a></div>
+                </div>
+            </section>
+        </section>
+      </>
      );
   }
 }
